7e2d31e878d2303160bbfb29257d09b5388e6882
Itzel <Itzel@10.20.4.3>
2017-01-18 13:25:56 -0600

    session de arreglos creamos una arreglo de alumnos

---
+++ src/sesiones/ArreglosAlumnos.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesiones;
+
+/**
+ *
+ * @author Itzel
+ */
+public class ArreglosAlumnos {  //refactor cambiar todo el nombre
+    
+    //atributos o fields o class variables
+    private String [] alumnos;
+    private int contador;
+    
+    
+    //metodos
+    public void inicializaArreglo(int size) {
+        
+        alumnos = new String[size];
+        contador = 0;
+    }
+    
+    public void agregarNombre(String nombre) {
+        
+        alumnos[contador] = nombre;
+        contador = contador + 1; //contador++;
+    }
+    
+    public void imprimirArreglo() {
+        for (int i = 1; i < contador; i++) {
+            
+            System.out.println("Nombre " + (i+1) + " " + alumnos[i]);
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        //declaro variable de tipo clase arreglos
+        ArreglosAlumnos alumnosUDLA;
+        //reservo espacio para la instancia
+        alumnosUDLA = new ArreglosAlumnos();
+        
+        alumnosUDLA.inicializaArreglo(10);
+        alumnosUDLA.agregarNombre("Pedrito");
+        alumnosUDLA.agregarNombre("Rodrigo");
+        alumnosUDLA.agregarNombre("Roberto");
+        alumnosUDLA.imprimirArreglo();
+    }
+}
