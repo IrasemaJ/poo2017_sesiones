ff488d8da26e568ecb2ab191f1e1538359ccc341
Itzel <Itzel@10.20.4.3>
2017-04-19 13:26:44 -0500

    Interface Grafica

---
+++ src/InterfacesGraficas/MiVentana.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfacesGraficas;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Itzel
+ */
+public class MiVentana extends JFrame{
+    
+    public static void main(String[] args) {
+        MiVentana mv = new MiVentana();
+        mv.setSize(500, 500);
+        mv.setVisible(true);
+    }
+    
+}
