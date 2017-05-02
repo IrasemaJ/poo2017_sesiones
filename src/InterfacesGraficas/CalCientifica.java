a914f1b4cee485340f522cfd56a205bc69c2a848
Itzel <Itzel@10.20.4.3>
2017-05-01 19:16:34 -0500

    calculadora cientifica

--- src/InterfacesGraficas/CalCientifica.java
+++ src/InterfacesGraficas/CalCientifica.java
@@ -7,6 +7,7 @@ package InterfacesGraficas;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.Action;
 
 /**
  *
@@ -16,42 +17,108 @@ public class CalCientifica extends javax.swing.JFrame {
 
     protected int value1;
     protected int value2;
+    protected String operacion = "";
     /**
      * Creates new form CalCientifica
      */
+    protected void getFirstValue() {
+        Integer i = new Integer(texto.getText());
+        value1 = i.intValue();
+        texto.setText("");
+    }
+    
+    protected void getSecondValue(){
+        Integer i = new Integer(texto.getText());
+        value2 = i.intValue();
+        texto.setText("");
+        
+    }
+    
+    
     public CalCientifica() {
         initComponents();
-        jButton1.addActionListener(new ActionListener() {
+        
+        c.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                jTextField1.setText("");
+                texto.setText("");
             }
         });
-        jButton9.addActionListener(new ActionListener() {
+    
+        dos.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                jTextField1.setText(jTextField1.getText() + "2");
+                texto.setText(texto.getText() + "2");
             }
         });
-        jButton3.addActionListener(new ActionListener() {
+        suma.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Integer i = new Integer(jTextField1.getText());
-                value1 = i.intValue();
-                jTextField1.setText("");
+                if (operacion.equals("")){
+                    
+                    getFirstValue();
+                    operacion = suma.getActionCommand();
+                }
+                else{
+                    getSecondValue();
+                    texto.setText("");
+                    value1 = value1 + value2;
+                    Integer i = new Integer(value1);
+                    texto.setText(i.toString());
+                    operacion = suma.getActionCommand();
+                }
             }
                 
         });
-        jButton10.addActionListener(new ActionListener() {
+        resta.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Integer ii = new Integer(jTextField1.getText());
+                getFirstValue();
+                operacion = resta.getActionCommand();
+            }
+        });
+        multiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getFirstValue();
+                operacion = multiplicacion.getActionCommand();
+            }
+        });
+        division.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getFirstValue();
+                operacion = division.getActionCommand();
+            }
+        });
+        igual.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int resultado = 0;
+                Integer ii = new Integer(texto.getText());
                 value2 = ii.intValue();
                 
-                int resultado = value2 + value2;
-                
-                jTextField1.setText(resultado + "");
-                
+                if (operacion.equals(suma.getActionCommand())) {
+                    resultado = value1 + value2;
+                }
+                if (operacion.equals(resta.getActionCommand())) {
+                    resultado = value1 - value2;
+                }
+                if (operacion.equals(division.getActionCommand())) {
+                    if (value2 !=0) {
+                        
+                        resultado = value1 / value2;
+                    }
+                else{
+                        throw new Zerodivison();
+                    }
+                         
+                }
+                if (operacion.equals(multiplicacion.getActionCommand())) {
+                    resultado = value1 * value2;
+                }
+                texto.setText(resultado + ""); 
+                operacion = "";
             }
         });
         
@@ -67,47 +134,47 @@ public class CalCientifica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
+        texto = new javax.swing.JTextField();
+        c = new javax.swing.JButton();
+        borrar = new javax.swing.JButton();
+        suma = new javax.swing.JButton();
+        resta = new javax.swing.JButton();
+        multiplicacion = new javax.swing.JButton();
+        division = new javax.swing.JButton();
+        uno = new javax.swing.JButton();
+        dos = new javax.swing.JButton();
+        igual = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("C");
-        jButton1.setActionCommand("Clear");
+        c.setText("C");
+        c.setActionCommand("Clear");
 
-        jButton2.setText("<<");
-        jButton2.setActionCommand("Back");
+        borrar.setText("<<");
+        borrar.setActionCommand("Back");
 
-        jButton3.setText("+");
-        jButton3.setActionCommand("Suma");
+        suma.setText("+");
+        suma.setActionCommand("Suma");
 
-        jButton5.setText("-");
-        jButton5.setActionCommand("Resta");
+        resta.setText("-");
+        resta.setActionCommand("Resta");
 
-        jButton6.setText("*");
-        jButton6.setActionCommand("Multipl");
+        multiplicacion.setText("*");
+        multiplicacion.setActionCommand("Multipl");
 
-        jButton7.setText("/");
-        jButton7.setActionCommand("Division");
+        division.setText("/");
+        division.setActionCommand("Division");
 
-        jButton8.setText("1");
-        jButton8.addActionListener(new java.awt.event.ActionListener() {
+        uno.setText("1");
+        uno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton8ActionPerformed(evt);
+                unoActionPerformed(evt);
             }
         });
 
-        jButton9.setText("2");
+        dos.setText("2");
 
-        jButton10.setText("=");
+        igual.setText("=");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -117,58 +184,58 @@ public class CalCientifica extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(40, 40, 40)
-                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(uno, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(79, 79, 79)
-                        .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(dos, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(110, 110, 110)
-                        .addComponent(jButton10, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(igual, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 473, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(texto, javax.swing.GroupLayout.PREFERRED_SIZE, 473, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(c, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)
+                                .addComponent(borrar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton3)
+                                .addComponent(suma)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton5)
+                                .addComponent(resta)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton6)
+                                .addComponent(multiplicacion)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton7)))))
+                                .addComponent(division)))))
                 .addContainerGap(28, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(texto, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jButton5)
-                    .addComponent(jButton6)
-                    .addComponent(jButton7))
+                    .addComponent(c, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(borrar)
+                    .addComponent(suma)
+                    .addComponent(resta)
+                    .addComponent(multiplicacion)
+                    .addComponent(division))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton8, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
-                    .addComponent(jButton9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(uno, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
+                    .addComponent(dos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
-                .addComponent(jButton10, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(igual, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        jTextField1.setText(jTextField1.getText() + "1");
-    }//GEN-LAST:event_jButton8ActionPerformed
+    private void unoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_unoActionPerformed
+        texto.setText(texto.getText() + "1");
+    }//GEN-LAST:event_unoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -207,15 +274,15 @@ public class CalCientifica extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton borrar;
+    private javax.swing.JButton c;
+    private javax.swing.JButton division;
+    private javax.swing.JButton dos;
+    private javax.swing.JButton igual;
+    private javax.swing.JButton multiplicacion;
+    private javax.swing.JButton resta;
+    private javax.swing.JButton suma;
+    private javax.swing.JTextField texto;
+    private javax.swing.JButton uno;
     // End of variables declaration//GEN-END:variables
 }
